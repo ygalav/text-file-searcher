57393d097512637e26b988193f14c584fe3491a5
ygalavay <ygalavay@gmail.com>
2020-03-07 13:25:13 +0100

    Setup Stub Maven project

---
+++ src/main/java/com/ygalav/ftssearcher/Searcher.java
@@ -0,0 +1,9 @@
+package com.ygalav.ftssearcher;
+
+import java.util.Arrays;
+
+public class Searcher {
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
+}
