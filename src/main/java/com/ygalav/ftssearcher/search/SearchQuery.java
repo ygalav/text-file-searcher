21bf4438f40308bad77f57bdac007fad537708d1
ygalavay <ygalavay@gmail.com>
2020-03-09 14:20:26 +0100

    Added search query basic fields

--- src/main/java/com/ygalav/ftssearcher/search/SearchQuery.java
+++ src/main/java/com/ygalav/ftssearcher/search/SearchQuery.java
@@ -4,4 +4,20 @@ package com.ygalav.ftssearcher.search;
  * Represents the search query, can contain meta-data [pagination, etc...]
  */
 public class SearchQuery {
+
+    private String text;
+    private int limit;
+
+    public SearchQuery(String text, int limit) {
+        this.text = text;
+        this.limit = limit;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
 }
